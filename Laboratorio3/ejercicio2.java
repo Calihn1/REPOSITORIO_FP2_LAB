27ba8895865d948e1dadc2579a90d9ba5b62cc1b
Calihn1 <ehilacondob@unsa.edu.pe>
2024-10-05 13:08:47 -0500

    Segundo avance del codigo

--- Laboratorio3/ejercicio2.java
+++ Laboratorio3/ejercicio2.java
@@ -1,5 +1,24 @@
 package Laboratorio3;
 
+import java.util.*;
 public class ejercicio2 {
-
+    public static void main(String[] args) {
+        Soldados[] soldados= new Soldados[5];
+        rellenarLista(soldados);
+        imprimirLista(soldados);
+    }
+    public static void rellenarLista(Soldados[] lista) {
+        Scanner sc=new Scanner(System.in);
+        for(int i=0; i<lista.length; i++) {
+            lista[i]=new Soldados();
+            System.out.print("Ingrese su nombre: ");
+            lista[i].setNombre(sc.next());
+            System.out.print("Ingrese el nivel de vida: ");
+            lista[i].setVida(sc.nextInt());
+        }
+    }
+    public static void imprimirLista(Soldados[] lista) {
+        for(Soldados n: lista)
+        System.out.println(n);
+    }
 }
