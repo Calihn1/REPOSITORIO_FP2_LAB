a89e332c4b6c5afa8bf481cf860592e7b4462d45
Calihn1 <ehilacondob@unsa.edu.pe>
2024-10-05 13:09:15 -0500

    Segundo avance del codigo

--- Laboratorio3/Soldados.java
+++ Laboratorio3/Soldados.java
@@ -1,5 +1,23 @@
 package Laboratorio3;
 
 public class Soldados {
+	private int vida;
+    private String nombre;
+
+    public void setNombre( String nombre_) {
+        nombre=nombre_;
+    }
+    public void setVida( int vida_) {
+        vida=vida_;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getVida() {
+        return vida;
+    }
+    public String toString() {
+        return ("Nombre: "+getNombre()+"\nVida: "+getVida()+"\n");
+    }
 
 }
